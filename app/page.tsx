8430adf50cdf347d7d3e98faaf443d735f528df9
Gustavo Kleist <gustavokleist@icloud.com>
2023-06-19 19:44:47 +0100

    update on text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hey I am a new page </h1>
+      <h1>Hey I am a new page, now automatically deployed </h1>
     </main>
   )
 }
